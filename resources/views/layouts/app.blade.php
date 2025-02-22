5eede51fe80b0be6a95e4438c2fe5c99674c8380
doge <doge@gmail.com>
2025-02-23 00:06:41 +0800

    input padding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,13 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
        <!-- Styles -->
        <script src="https://cdn.tailwindcss.com"></script>
        <!-- Alpine.js -->
-       <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+       <script defer src="https://unpkg.com/browse/alpinejs@3.14.8/dist/cdn.min.js"></script>
        <script>
            function handleImageSelect(input) {
                const previewGrid = document.getElementById('imagePreviewGrid');
