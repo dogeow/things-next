a2538d620c607939d497d55efbbe993f02b4dc6c
doge <doge@gmail.com>
2025-02-22 19:41:58 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ItemController::class, 'plaza'])->name('plaza');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
